46f1197ddea746dd3ef472c5bbf1cbe937dd05d2
Carlos. APT <carlospt_dev@outlook.com>
2024-06-25 12:03:28 -0500

    fix: Corregit espacios en las funciones

--- script/script.js
+++ script/script.js
@@ -83,12 +83,12 @@ function jorge(){
 
 //-------------------------------------------------------
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
+  document.getElementById("myForm").style.display = "block";
+}
   
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
 
 
@@ -99,50 +99,50 @@ function openForm() {
 
 dragElement(document.getElementById("mydiv"));
     
-    function dragElement(elmnt) {
-      var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-      if (document.getElementById(elmnt.id + "header")) {
-        /* if present, the header is where you move the DIV from:*/
-        /* si está presente, el encabezado es desde donde mueves el DIV:*/
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-      } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        /* de lo contrario, mueva el DIV desde cualquier lugar dentro del DIV:*/
-        elmnt.onmousedown = dragMouseDown;
-      }
-    
-      function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        // obtiene la posición del cursor del mouse al inicio:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        // llama a una función cada vez que se mueve el cursor:
-        document.onmousemove = elementDrag;
-      }
-    
-      function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        // calcula la nueva posición del cursor:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        // establece la nueva posición del elemento:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
-    
-      function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        /* deja de moverse cuando se suelta el botón del mouse:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-      }
-    }
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    /* si está presente, el encabezado es desde donde mueves el DIV:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    /* de lo contrario, mueva el DIV desde cualquier lugar dentro del DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    // obtiene la posición del cursor del mouse al inicio:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    // llama a una función cada vez que se mueve el cursor:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    // calcula la nueva posición del cursor:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    // establece la nueva posición del elemento:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    /* deja de moverse cuando se suelta el botón del mouse:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
