bf4fed1be85be777bc4d082e6a3966d4bb5d97cb
Carlos. APT <carlospt_dev@outlook.com>
2024-07-01 11:37:16 -0500

    Fix: Arreglar el formulario de búsqueda para que sea arrastrable y seleccionable, OK

--- script/close_form.js
+++ script/close_form.js
@@ -11,73 +11,59 @@ function openForm(form_clas) {
       form_element.style.display = "block";
       dragElement(form_element);
       break;
-  }
-}
-  
-function closeForm(formulario_clase) {
-  const form_element = document.querySelector(formulario_clase)
-  switch(form_element.className) {
-    case "form-box form-box-busqueda":
-      console.log("CLOSE BUSQUEDA: ", form_element.className);
-      form_element.style.display = "none";
+    }
+    }
+    
+    function closeForm(formulario_clase) {
+      const form_element = document.querySelector(formulario_clase)
+      switch(form_element.className) {
+        case "form-box form-box-busqueda":
+          form_element.style.display = "none";
       break;
     case "form-box form-box-geodata":
-      console.log("CLOSE GEODATA: ", form_element.className);
       form_element.style.display = "none";
       break;
   }
 }
-
 
 //------------------------ Arrastre DEL DIV --------------------------------------
 //--------------------------------------------------------------------------------
 // Make the DIV element draggagle:
 
-    
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (elmnt) {
-    /* if present, the header is where you move the DIV from:*/
-    /* si está presente, el encabezado es desde donde mueves el DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    /* de lo contrario, mueva el DIV desde cualquier lugar dentro del DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+  // const d_element = document.querySelector("form-header__search")
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.addEventListener('mousedown', dragMouseDown);
 
   function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
     // get the mouse cursor position at startup:
     // obtiene la posición del cursor del mouse al inicio:
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    document.addEventListener('mouseup', closeDragElement)
     // call a function whenever the cursor moves:
     // llama a una función cada vez que se mueve el cursor:
-    document.onmousemove = elementDrag;
+    document.addEventListener('mousemove', elementDrag)
+
+    function elementDrag(e) {
+      // calculate the new cursor position:
+      // calcula la nueva posición del cursor:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      // establece la nueva posición del elemento:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+    
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      /* deja de moverse cuando se suelta el botón del mouse:*/
+      document.removeEventListener('mousemove', elementDrag)
+    }
   }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    // calcula la nueva posición del cursor:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    // establece la nueva posición del elemento:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    /* deja de moverse cuando se suelta el botón del mouse:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
 }
+
